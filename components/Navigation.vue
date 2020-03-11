247c1b83ac860b3213531551fe31a76ebb73f54e
Brent Miller <brentwmiller93@gmail.com>
2020-03-10 23:24:14 -0400

    Sets up base files for testing

---
+++ components/Navigation.vue
@@ -0,0 +1,20 @@
+<template>
+  <header>
+    <div class="container flex justify-between align-middle mx-auto mt-4">
+      <div class="text-xl font-bold">brent</div>
+      <nav class="flex align-middle">
+        <ul class="flex align-middle">
+          <li class="px-4 py-2">
+            <a href="#">Projects</a>
+          </li>
+          <li class="px-4 py-2">
+            <a href="#">Articles</a>
+          </li>
+          <li class="pl-4 py-2">
+            <a href="#">Experience</a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  </header>
+</template>

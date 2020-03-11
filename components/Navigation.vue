b62780e62ad5bb8b968553b8f1ac730a3fbaf8ea
Brent Miller <brentwmiller93@gmail.com>
2020-03-10 23:34:28 -0400

    Sets up the base nav

--- components/Navigation.vue
+++ components/Navigation.vue
@@ -1,17 +1,23 @@
 <template>
   <header>
     <div class="container flex justify-between align-middle mx-auto mt-4">
-      <div class="text-xl font-bold">brent</div>
+      <div class="text-xl font-semibold">brent</div>
       <nav class="flex align-middle">
         <ul class="flex align-middle">
           <li class="px-4 py-2">
-            <a href="#">Projects</a>
+            <a href="#" class="uppercase tracking-wider text-base">
+              Projects
+            </a>
           </li>
           <li class="px-4 py-2">
-            <a href="#">Articles</a>
+            <a href="#" class="uppercase tracking-wider text-base">
+              Articles
+            </a>
           </li>
           <li class="pl-4 py-2">
-            <a href="#">Experience</a>
+            <a href="#" class="uppercase tracking-wider text-base">
+              Experience
+            </a>
           </li>
         </ul>
       </nav>

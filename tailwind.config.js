576816c24d630be99e1715b25cb75ba94f223348
Brent Miller <brentwmiller93@gmail.com>
2020-03-10 23:23:55 -0400

    Adds Nunito font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,17 @@
 /*
-** TailwindCSS Configuration File
-**
-** Docs: https://tailwindcss.com/docs/configuration
-** Default: https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
-*/
+ ** TailwindCSS Configuration File
+ **
+ ** Docs: https://tailwindcss.com/docs/configuration
+ ** Default: https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
+ */
 module.exports = {
-  theme: {},
+  important: false,
+  theme: {
+    fontFamily: {
+      body: ['Nunito'],
+      display: ['Nunito']
+    }
+  },
   variants: {},
   plugins: []
 }

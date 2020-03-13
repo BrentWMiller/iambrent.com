b37c3c11fe0e76d2cf5e0a3b848aff6c17aa9cd5
Brent Miller <brentwmiller93@gmail.com>
2020-03-12 23:35:13 -0400

    recolors and adds copy/accents

--- components/Hero/Hero.vue
+++ components/Hero/Hero.vue
@@ -1,8 +1,32 @@
 <template>
-  <section class="container mx-auto">
-    <h1 class="text-8xl font-extrabold leading-none text-primary">
-      I'm a<br />
-      <span class="text-accent">web developer</span>.
-    </h1>
+  <section class="container flex items-center justify-center h-full mx-auto">
+    <div class="absolute top-0 left-0 w-1/2 h-full -ml-12 bg-secondary-300"></div>
+    <div class="relative flex flex-col">
+      <h1 class="font-extrabold leading-none text-8xl text-primary">
+        I'm a
+        <br />
+        <span class="text-secondary-500">web developer</span>,
+      </h1>
+      <p class="mt-4 text-2xl">
+        building web sites and web apps for
+        <a
+          class="font-bold hover:underline text-primary"
+          href="https://skycatchfire.com"
+          target="_blank"
+        >SKYCATCHFIRE</a>.
+      </p>
+    </div>
+    <dots class="absolute top-9/12 left-10/12" />
+    <dots class="absolute top-2/12 left-5/12" />
   </section>
 </template>
+
+<script>
+import dots from '~/components/dots/dots.vue'
+
+export default {
+  components: {
+    dots
+  }
+}
+</script>

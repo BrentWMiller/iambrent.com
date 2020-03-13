b6e640d80edc98f1d4e34e0b37ee82394404b869
Brent Miller <brentwmiller93@gmail.com>
2020-03-13 00:05:41 -0400

    Adds footer; Reworks hero grow; adds test section below

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,9 +1,13 @@
 <template>
-  <div class="flex flex-col h-screen font-body text-dark-primary">
+  <div>
     <navigation />
-    <div class="flex-grow">
+    <div class="font-body text-dark-primary">
       <nuxt />
     </div>
+    <footer class="container flex justify-between w-full py-3 mx-auto text-xs text-gray-500 h-half">
+      <span>Built with Vue, Tailwind CSS, and some sweet tunes</span>
+      <span>&copy; Brent Miller</span>
+    </footer>
   </div>
 </template>
 

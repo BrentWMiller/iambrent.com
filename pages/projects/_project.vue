c88511a06e348a8f4a5636c2b72924a7eb70b62b
Brent Miller <brentwmiller93@gmail.com>
2020-03-14 00:18:07 -0400

    Sets up project index/pages

---
+++ pages/projects/_project.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="mt-24">
+    <div class="container w-5/12 mx-auto mb-20 text-center">
+      <h2 class="mb-2 text-lg font-semibold uppercase text-secondary-500">
+        {{ project.title }}
+      </h2>
+      <h3 class="mb-4 text-4xl font-semibold leading-tight text-primary">
+        {{ project.description }}
+      </h3>
+      <p class="text-dark-primary">{{ project.body }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {},
+  async asyncData({ params, payload }) {
+    if (payload) return { project: payload }
+    else
+      return {
+        project: await require(`~/assets/content/project/${params.project}.json`)
+      }
+  }
+}
+</script>
--- pages/projects/index.vue
+++ pages/projects/index.vue
@@ -1,13 +1,21 @@
 <template>
   <div>
-    <div class="container w-5/12 mx-auto mb-20 text-center">
-      <h2 class="mb-2 text-lg font-semibold uppercase text-secondary-500">
-        Test
-      </h2>
-      <h3 class="mb-4 text-4xl font-semibold leading-tight text-primary">
-        Test
-      </h3>
-      <p class="text-dark-primary">Test</p>
-    </div>
+    <ul class="w-1/2 mx-auto my-24">
+      <li v-for="project in projects" :key="project.slug">
+        <nuxt-link :to="`projects/${project.slug}`">{{
+          project.title
+        }}</nuxt-link>
+      </li>
+    </ul>
   </div>
 </template>
+
+<script>
+export default {
+  computed: {
+    projects() {
+      return this.$store.state.projects
+    }
+  }
+}
+</script>

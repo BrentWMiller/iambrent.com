73b86d44e6c73c7b0a67455e54d9760889bc8e89
Brent Miller <brentwmiller93@gmail.com>
2020-03-14 23:52:06 -0400

    Updates the footer

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,11 +2,11 @@
   <div class="flex flex-col min-h-screen font-body text-dark-primary">
     <Navigation />
     <nuxt class="flex-grow" />
-    <footer
-      class="container flex justify-between w-full py-3 mx-auto text-xs text-gray-500 h-half"
-    >
-      <span>Built with Vue, Tailwind CSS, and some sweet tunes</span>
-      <span>&copy; Brent Miller</span>
+    <footer class="w-full py-12 bg-primary h-half">
+      <div class="container flex justify-between mx-auto text-xs text-secondary-500">
+        <span>Built with Vue, Tailwind CSS, and some sweet tunes</span>
+        <span>&copy; Brent Miller</span>
+      </div>
     </footer>
   </div>
 </template>

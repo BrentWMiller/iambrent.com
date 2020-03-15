f7cb6d6f406e4cb7e9c71ea590c2febf28d3eaad
Brent Miller <brentwmiller93@gmail.com>
2020-03-15 01:02:54 -0400

    Starts building the projects section

--- pages/index.vue
+++ pages/index.vue
@@ -4,20 +4,49 @@
       <hero />
     </div>
 
-    <ul class="container mx-auto my-24">
-      <li v-for="project in projects" :key="project.slug">
-        <nuxt-link :to="`projects/${project.slug}`">{{ project.title }}</nuxt-link>
-      </li>
-    </ul>
+    <div class="container mx-auto my-40">
+      <div class="w-10/12 mx-auto">
+        <h2
+          class="relative flex items-center text-lg font-bold tracking-wider uppercase text-secondary-500"
+        >
+          <dots class="absolute left-0 -mt-4 -ml-16 -z-1" />Projects
+        </h2>
+        <h3 class="text-5xl font-bold text-primary">Some of my favorites</h3>
+        <ul class="flex">
+          <li v-for="project in projects" :key="project.slug" class="block p-3 border">
+            <nuxt-link :to="`projects/${project.slug}`">{{ project.title }}</nuxt-link>
+          </li>
+        </ul>
+
+        <nuxt-link
+          to="/projects"
+          class="inline-flex items-center px-6 py-3 mt-6 text-lg font-bold text-primary bg-accent"
+        >
+          See all of my projects
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            class="w-8 ml-4"
+          >
+            <path
+              class="text-primary"
+              d="M14.59 13H7a1 1 0 010-2h7.59l-2.3-2.3a1 1 0 111.42-1.4l4 4a1 1 0 010 1.4l-4 4a1 1 0 01-1.42-1.4l2.3-2.3z"
+            />
+          </svg>
+        </nuxt-link>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import hero from '~/components/Hero'
+import dots from '~/components/Dots'
 
 export default {
   components: {
-    hero
+    hero,
+    dots
   },
   head() {
     return {

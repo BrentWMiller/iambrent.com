68c423c0a868edbef9be8842e6d21f83411f269a
Brent Miller <brentwmiller93@gmail.com>
2021-08-17 22:32:30 -0400

    💄 opens in new tab

--- components/SocialAndContact.vue
+++ components/SocialAndContact.vue
@@ -4,8 +4,8 @@
     <div class="space-y-4">
       <p class="code-block">// check out my code</p>
       <ul class="space-y-4 text-4xl font-medium text-white">
-        <li><a class="link" href="https://github.com/BrentWMiller/">github</a></li>
-        <li><a class="link" href="https://codepen.io/BrentWMiller/">codepen</a></li>
+        <li><a class="link" href="https://github.com/BrentWMiller/" target="_blank">github</a></li>
+        <li><a class="link" href="https://codepen.io/BrentWMiller/" target="_blank">codepen</a></li>
       </ul>
     </div>
 

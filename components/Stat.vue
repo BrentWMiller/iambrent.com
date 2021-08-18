08dfeeec920502784cecf4cfb9af7a9cbdcde65a
Brent Miller <brentwmiller93@gmail.com>
2021-08-17 22:50:37 -0400

    💄 adds base stats

---
+++ components/Stat.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h3 class="flex items-center space-x-4">
+      <span :class="`block text-8xl font-bold ${stat.color}`">{{ stat.value }}+</span>
+      <span class="font-medium text-white max-w-[6.75rem] block">{{ stat.label }}</span>
+    </h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Stat',
+  props: {
+    stat: {
+      type: Object
+    },
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ components/Stats.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="container">
+
+    <div class="max-w-screen-lg mx-auto">
+      <p class="mb-8 ml-8 code-block">for stat in stats</p>
+      
+      <ul class="grid grid-cols-3 gap-24">
+        <li v-for="stat, index in stats" :key="index">
+          <stat :stat="stat" />
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      stats: [
+        {
+          value: 6,
+          label: "Years of Experiece",
+          color: 'text-darkula-green'
+        },
+        {
+          value: 50,
+          label: "Websites",
+          color: 'text-darkula-yellow'
+        },
+        {
+          value: 5,
+          label: "Web apps",
+          color: 'text-darkula-pink'
+        },
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>

5c4ed992a8b982cd957bedb2c01bf550a5674943
BrentWMiller <brentwmiller93@gmail.com>
2021-12-17 00:30:10 -0500

    💄 Updates intro to include SKY; moves hello() first

--- components/Intro.vue
+++ components/Intro.vue
@@ -3,13 +3,14 @@
     class="container space-y-4"
   >
     <p class="code-block">
+      hello()<br />
       let buzzword = "<code-input :initial-value="buzzword" @update="setBuzzword"/>"<br />
-      hello()
     </p>
 
     <h1 class="max-w-4xl dark:text-white heading-1">
       Building <span class="text-lightula-green dark:text-darkula-green">{{ buzzword }}</span> websites and apps.
     </h1>
+    <p class="code-block">with the team at <a href="https://www.skycatchfire.com/" rel="noreferrer" class="button dark:hover:text-darkula-green hover:text-lightula-green">SKYCATCHFIRE</a></p>
   </section>
 </template>
 

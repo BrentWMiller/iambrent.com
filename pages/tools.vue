7f8f672d13f9110975d488fa64bcea68ea64f4a1
BrentWMiller <brentwmiller93@gmail.com>
2021-12-23 16:59:36 -0500

    🚧  Starts building out tools

---
+++ pages/tools.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="container space-y-16 text-black dark:text-white">
+    <div class="space-y-2">
+      <h1 class="heading-2">Tools</h1>
+      <p class="code-block">// Apps, setup, whatever I use all the time.</p>
+    </div>
+
+    <div>
+      <h2>Apps</h2>
+      <p>I use these apps almost every day for work and life.</p>
+
+      <ul>
+        <li v-for="app, index in apps" :key="index">
+          <a :href="app.url" target="_blank" rel="noopener" title="View website">{{ app.name }}</a>
+        </li>
+      </ul>
+    </div>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Tools',
+  data() {
+    return {
+      apps: {
+        vs_code: {
+          name: 'VS Code',
+          url: 'https://code.visualstudio.com?ref=iambrent',
+        },
+        craft: {
+          name: 'Craft',
+          url: 'https://www.craft.do?ref=iambrent',
+        }
+      }
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>

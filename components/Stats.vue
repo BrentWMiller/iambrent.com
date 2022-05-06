27668294a1498d0b63aab3357e660c12bcfe8222
BrentWMiller <brentwmiller93@gmail.com>
2022-05-05 23:48:10 -0400

    💬 Updates stats

--- components/Stats.vue
+++ components/Stats.vue
@@ -1,35 +1,44 @@
 <template>
   <div class="container relative pt-0 md:pt-36">
+    <h2 class="max-w-lg mx-auto text-center mb-14 heading-2 dark:text-white">
+      With the experience to back it up.
+    </h2>
 
-    <h2 class="max-w-lg mx-auto text-center mb-14 heading-2 dark:text-white">With the experience to back it up.</h2>
-
-<div class="absolute -left-20 top-36 md:-top-64 code-block">
-const stats = [
-<template v-for="stat, index in stats"><pre :key="index">
+    <div class="absolute -left-20 top-36 md:-top-64 code-block">
+      const stats = [
+      <template v-for="(stat, index) in stats">
+        <pre :key="index">
   {
     value: <code-input :color="stat.color" :initial-value="stat.value" :name="stat.label" @update="updateStat($event, index)" />,
     label: {{ stat.label }}
   }<span v-if="index < stats.length - 1">,</span>
-</pre></template>
-]
-</div>
+</pre>
+      </template>
+      ]
+    </div>
 
-<pre class="absolute top-0 -right-12 sm:right-0 code-block" aria-hidden="true">
+    <pre
+      class="absolute top-0 -right-12 sm:right-0 code-block"
+      aria-hidden="true"
+    >
 {
   value: Number,
   label: String
 }
-</pre>
+</pre
+    >
 
     <div class="relative z-10 max-w-screen-lg mx-auto">
-      <ul v-if="stats" class="flex flex-col items-center justify-center space-y-16 md:flex-row md:space-y-0 md:space-x-12 lg:space-x-24">
-        <li v-for="stat, index in stats" :key="index">
+      <ul
+        v-if="stats"
+        class="flex flex-col items-center justify-center space-y-16 md:flex-row md:space-y-0 md:space-x-12 lg:space-x-24"
+      >
+        <li v-for="(stat, index) in stats" :key="index">
           <stat :stat="stat" />
         </li>
       </ul>
       <p class="mt-8 ml-8 code-block" aria-hidden="true">for stat in stats</p>
     </div>
-    
   </div>
 </template>
 
@@ -41,33 +50,31 @@ export default {
         {
           value: new Date().getFullYear() - 2015,
           label: "Years of Experience",
-          color: 'text-lightula-green dark:text-darkula-green',
-          suffix: ''
+          color: "text-lightula-green dark:text-darkula-green",
+          suffix: "+",
         },
         {
-          value: 50,
+          value: 60,
           label: "Websites",
-          color: 'text-lightula-yellow dark:text-darkula-yellow',
-          suffix: '+'
+          color: "text-lightula-yellow dark:text-darkula-yellow",
+          suffix: "+",
         },
         {
-          value: 5,
+          value: 10,
           label: "Web apps",
-          color: 'text-lightula-pink dark:text-darkula-pink',
-          suffix: '+'
+          color: "text-lightula-pink dark:text-darkula-pink",
+          suffix: "+",
         },
-      ]
-    }
+      ],
+    };
   },
   methods: {
     updateStat(value, index) {
       console.log(value, index);
       this.stats[index].value = value ? value : 0;
-    }
-  }
-}
+    },
+  },
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
